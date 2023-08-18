fcb356a844dccaa82db7c65bf76b41bf00f7f8a4
maksimrepp <reppmg@gmail.com>
2023-08-18 15:11:09 +0400

    init

---
+++ app/src/main/java/repp/max/cloudcue/screens/CityWeatherList.kt
@@ -0,0 +1,13 @@
+package repp.max.cloudcue.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun CityWeatherListScreen(
+    navigation: Navigation,
+    viewModel: CityWeatherViewModel
+) {
+    Text(text = "suppose you see a list")
+}
